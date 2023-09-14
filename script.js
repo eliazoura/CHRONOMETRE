900f2b439ba148c440f340080855db3236b9d509
Eli Azoura <51091899+eliazoura@users.noreply.github.com>
2023-09-14 11:54:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,112 @@
-let resetEl=document.getElementById("reset");
+body {
+   margin: 0px 0px 0px 0px;
+   box-sizing: border-box;
+   padding: 0%;
 
-let stopEli=document.getElementById("stop");
+   background-color: rgb(20, 12, 0);
+   
+   
+   background-color: antiquewhite;
+margin: 100px 0px 0px 0px;
 
-let startEl=document.getElementById("start");
+   background-color: antiquewhite;
+margin: 100px 0px 0px 0px;
 
+   /*corriger le margin top pour qu'il se cale automatiquement au centre de 'window' */
+
+}
+
+
+
+.ensemble {
+   display: grid;
+   grid-template-rows: 3fr 2fr 3fr;
+}
+
+
+
+.box {
+
+   display: grid;
+   grid-template-rows: 3fr 1fr;
+
+
+
+   background-color: rgb(150, 134, 113);
+
+   width: 80%;
+   height: 100px;
+   margin: auto;
+
+   text-align: center;
+   color: antiquewhite;
+
+   border-radius: 20px;
+
+}
+
+
+
+.act1 {
+
+   width: 80px;
+   background-color: rgb(68, 68, 68);
+   border-radius: 10px;
+
+}
+
+
+
+.act2 {
+
+   width: 80px;
+   background-color: rgb(103, 62, 0);
+   border-radius: 10px;
+
+}
+
+
+
+.act3 {
+   color: rgb(255, 255, 255);
+   width: 80px;
+   background-color: rgba(255, 153, 0, 0.694);
+   border-radius: 10px;
+
+}
+
+.lesbtn {
+
+   display: grid;
+   align-items: center;
+
+   grid-template-columns: 1fr 1fr 1fr;
+   column-gap: 30px;
+
+   width: 100%;
+
+
+align-items: center;
+
+}
+
+
+
+.time {
+
+   font-size: xx-large;
+   background-color: rgb(68, 68, 68);
+   border-radius: 20px;
+   height: 70%;
+   box-shadow: 0px 5px 5px rgb(20, 12, 0);
+}
+
+
+
+.centre {
+
+
+   margin-left: 100px;
+   margin-right: 100px;
+
+}
